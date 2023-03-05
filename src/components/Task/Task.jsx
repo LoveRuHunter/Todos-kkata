57a6d93fbb66cb2c67f42873a6cf73c48bb0ff50
LoveRuHunter <113541018+LoveRuHunter@users.noreply.github.com>
2023-03-05 05:24:48 +0500

    update

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-// eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types'
 import KG from 'date-fns/locale/en-AU'
 import { formatDistanceToNow } from 'date-fns'
@@ -13,7 +12,6 @@ class Task extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const { editItem, id } = this.props
-    // eslint-disable-next-line react/destructuring-assignment
     editItem(id, this.state.value)
     this.setState({ editing: false })
   }
@@ -22,13 +20,10 @@ class Task extends Component {
     const { onDeleted, onToggleCompleted, label, id, completed, date } = this.props
 
     return (
-      // eslint-disable-next-line react/destructuring-assignment,no-nested-ternary
       <li className={completed ? 'completed' : this.state.editing ? 'editing' : null}>
         <div className="view">
           <input className="toggle" type="checkbox" checked={completed} onChange={onToggleCompleted} />
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
           <label>
-            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/click-events-have-key-events */}
             <span className="description" onClick={onToggleCompleted}>
               {label}
             </span>
@@ -40,29 +35,24 @@ class Task extends Component {
               })}`}
             </span>
           </label>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label,react/button-has-type */}
           <button
             className="icon icon-edit"
             onClick={() =>
               this.setState(({ editing }) => ({
                 editing: !editing,
-                // eslint-disable-next-line react/destructuring-assignment
                 value: this.props.label,
               }))
             }
             key={id}
           />
-          {/* eslint-disable-next-line react/button-has-type,jsx-a11y/control-has-associated-label */}
           <button className="icon icon-destroy" onClick={() => onDeleted(id)} />
         </div>
-        {/* eslint-disable-next-line react/destructuring-assignment */}
         {this.state.editing && (
           <form onSubmit={this.handleSubmit}>
             <input
               onChange={(e) => this.setState({ value: e.target.value })}
               type="text"
               className="edit"
-              /* eslint-disable-next-line react/destructuring-assignment */
               value={this.state.value}
             />
           </form>
@@ -73,16 +63,12 @@ class Task extends Component {
 }
 
 Task.propTypes = {
-  // eslint-disable-next-line react/require-default-props
   label: PropTypes.string,
   completed: PropTypes.bool,
-  // eslint-disable-next-line react/require-default-props
   id: PropTypes.number,
   onToggleCompleted: PropTypes.func.isRequired,
   onDeleted: PropTypes.func.isRequired,
-  // eslint-disable-next-line react/require-default-props
   date: PropTypes.instanceOf(Date),
-  // eslint-disable-next-line react/require-default-props
   editItem: PropTypes.func,
 }
 Task.defaultProps = {
